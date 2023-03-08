6626f8b9f581c8c9aba46c301756982f036d50e5
HongChangMo <hcm7402@naver.com>
2023-03-08 15:51:15 +0900

    first commit

---
+++ src/main/java/com/hcm/hcm_pj/HcmPjApplication.java
@@ -0,0 +1,13 @@
+package com.hcm.hcm_pj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcmPjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcmPjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hcm/hcm_pj/HcmPjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcm.hcm_pj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcmPjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
