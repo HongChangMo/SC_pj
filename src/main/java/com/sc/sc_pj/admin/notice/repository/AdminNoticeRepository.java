2c6217865104f6650563ad2abbcaa76db91188b2
HongChangMo <hcm7402@naver.com>
2023-03-17 23:31:17 +0900

    관리페이지 구조 세팅

---
+++ src/main/java/com/sc/sc_pj/admin/notice/repository/AdminNoticeRepository.java
@@ -0,0 +1,4 @@
+package com.sc.sc_pj.admin.notice.repository;
+
+public interface AdminNoticeRepository {
+}
---
+++ src/main/java/com/sc/sc_pj/admin/notice/service/AdminNoticeService.java
@@ -0,0 +1,7 @@
+package com.sc.sc_pj.admin.notice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminNoticeService {
+}
